1f094ad52d61c0824e6a651d407c8f729d793d8f
Gabe571 <gabriel.castro1028@gmail.com>
2020-09-05 20:35:42 -0700

    working migration/seed

--- db/migrate/20200904220619_create_users.rb
+++ db/migrate/20200904220619_create_users.rb
@@ -1,6 +1,6 @@
-class CreateUser < ActiveRecord::Migration[6.0]
+class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :user do |t|
+    create_table :users do |t|
       t.string :name
       t.string :bio
 
