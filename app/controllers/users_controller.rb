729b489e9032398a35a10996cd10fd2cf1c68ceb
Gabe571 <gabriel.castro1028@gmail.com>
2020-09-06 10:54:56 -0700

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+    
+    def index
+        users = User.all 
+        render json: users
+    end
 
+    def show
+        user = Users.find(params[:id])
+        render json: user
+    end
 end
 
