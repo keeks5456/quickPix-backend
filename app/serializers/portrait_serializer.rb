de88d5587029c8d075008bd8f2dd9ceea965661e
Gabe571 <gabriel.castro1028@gmail.com>
2020-09-09 11:10:08 -0700

    took out stuff in sera

--- app/serializers/portrait_serializer.rb
+++ app/serializers/portrait_serializer.rb
@@ -1,6 +1,6 @@
 class PortraitSerializer
   include FastJsonapi::ObjectSerializer
   attributes :img_url, :description, :like, :user_id, :comments
-  belongs_to: :user_id
-  has_many: :comments
+  # belongs_to: :user_id
+  # has_many: :comments
 end
